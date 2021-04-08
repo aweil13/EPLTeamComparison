2be584fa04354bd7b7888145b33c9dac6e8245fa
alejandro <alejandroweil13@gmail.com>
2021-04-08 02:58:40 -0400

    Working on line graph

--- src/index.js
+++ src/index.js
@@ -1,41 +1,112 @@
+
 import "./styles/index.scss";
 
-// dataset used for bars, their value is their height
-const data = [30, 800, 50, 200, 70];
+let teams = [];
+let seasonPoints = {};
+
+let seasonLength = [];
+
+for (let i = 0; i < 39; i++) {
+    seasonLength.push(i);    
+}
+
+d3.csv("./data/1819.csv")
+.then(data => {
+    // loop for extracting teams from season into teams array
+    for (let i = 0; i < data.length; i++) {
+        if (!teams.includes(data[i]["HomeTeam"])) {
+            teams.push(data[i]["HomeTeam"])
+        } else if (!teams.includes(data[i]["AwayTeam"])) {
+            teams.push(data[i]["AwayTeam"])
+        }
+    }
+    // nested loop for extracting team points from data
+    for (let i = 0; i < teams.length; i++) {
+        let team = teams[i];
+        seasonPoints[team] = [[0,0]];
+        for (let j = 0; j < data.length; j++) {
+            let match = data[j];
+            let prevPoints = seasonPoints[team][seasonPoints[team].length - 1][1]
+            let prevMatchday = seasonPoints[team][seasonPoints[team].length - 1][0]
+            // switch statements for building team points array
+            if (match["HomeTeam"] === team){
+                switch (match["FTR"]) {
+                    case "H":
+                        seasonPoints[team].push([prevMatchday + 1 ,prevPoints + 3]);
+                        break;
+                    case "A":
+                        seasonPoints[team].push([prevMatchday + 1 ,prevPoints]);
+                        break;
+                    case "D":
+                        seasonPoints[team].push([prevMatchday + 1 , prevPoints + 1]);    
+                    default:
+                        break;
+                }
+            } else if (match["AwayTeam"] === team){
+                switch (match["FTR"]) {
+                    case "A":
+                        seasonPoints[team].push([prevMatchday + 1 ,prevPoints + 3]);
+                        break;
+                    case "H":
+                        seasonPoints[team].push([prevMatchday + 1 ,prevPoints]);
+                        break;
+                    case "D":
+                        seasonPoints[team].push([prevMatchday + 1 , prevPoints + 1]);   
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+    
+//  Line Graph
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scaleLinear().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// var valueLine1 = d3.line(seasonPoints["Liverpool"])
+
+// var valueLine2 = d3.line(seasonPoints["Southampton"])
+
+var lineGenerator = d3.line();
+var pathData1 = lineGenerator(seasonPoints["Liverpool"])
+var pathData2 = lineGenerator(seasonPoints["Southampton"])
+
+var svg = d3.select("body").append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+x.domain([0, d3.max(seasonLength)])
+y.domain([0, d3.max(seasonPoints["Liverpool"][seasonPoints["Liverpool"].length - 1])]);
 
 
-// this is the width/height of container and the padding between bars
-const svgWidth = 500, svgHeight = 300, barPadding = 5;
 
-// bar width is the container wd
-const barWidth = (svgWidth/data.length)
+svg.append("path")
+.data(seasonPoints["Liverpool"])
+.attr("class", "line")
+.style("stroke", "red")
+.attr("d", pathData1);
 
- const svg = d3.select('svg')
-.attr("width", svgWidth)
-.attr("height", svgHeight);
+svg.append("path")
+.data(seasonPoints["Southampton"])
+.attr("d", pathData2)
+.attr("class", "line")
+.style("stroke", "blue")
 
- const barChart = svg.selectAll("rect")
-.data(data)
-.enter()
-.append("rect")
-.attr("y", (d) => { return svgHeight - d})
-.attr("height", (d) => {return d})        // bar height
-.attr("width", barWidth - barPadding)    // bar width 
-.attr("transform", (d, i) => {
-    const translate = [barWidth * i, 0];
-    return "translate("+ translate + ")";
-})
-.attr("fill", "yellow");
 
-const text = svg.selectAll("text")
-.data(data)
-.enter()
-.append("text")
-.text((d) => {return d;})
-.attr("y", (d, i) => {return svgHeight - d - 2;})
-.attr("x", (d, i) => {return barWidth * i + (barWidth/3)})
-.attr("fill", "red")
+svg.append("g")
+.attr("transform", "translate(0," + height + ")")
+.call(d3.axisBottom(x));
 
-const yScale = d3.scaleLinear()
-.domain([0, d3.max(data)])
-.range([0, svgHeight])
+svg.append("g").call(d3.axisLeft(y))
+
+});
+
+const arsenal = "Arsenal"
+console.log(seasonPoints)
+
