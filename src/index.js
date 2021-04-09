082982011840630589f86a6a260dd3934ee612e0
alejandro <alejandroweil13@gmail.com>
2021-04-09 14:12:51 -0400

    Animations for line graph are working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 
+import { path } from "d3-path";
 import "./styles/index.scss";
 
 let teams = [];
@@ -60,9 +61,9 @@ d3.csv("https://raw.githubusercontent.com/aweil13/EPLTeamComparison/main/data/18
     }
     
 //  Line Graph
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-width = 500 - margin.left - margin.right,
-height = 460 - margin.top - margin.bottom;
+var margin = {top: 20, right: 20, bottom: 70, left: 50},
+width = 660 - margin.left - margin.right,
+height = 600 - margin.top - margin.bottom;
 
 var svg = d3.select("body").append("svg")
 .attr("width", width + margin.left + margin.right)
@@ -83,14 +84,16 @@ y.domain([0, d3.max(seasonPoints["Liverpool"][seasonPoints["Liverpool"].length -
 
 svg.append("g")
 .attr("transform", "translate(0," + height + ")")
-.call(d3.axisBottom(x));
+.call(d3.axisBottom(x)).style("fill", "white")
 
-svg.append("g").call(d3.axisLeft(y))
+svg.append("g").call(d3.axisLeft(y)).style("fill", "white")
 
-    
+    // Lines and animation
+
     svg.append("path")
     .datum(seasonPoints["Liverpool"])
     .attr("class", "line")
+    .attr("id", "line0")
     .style("stroke", "red")
     .attr("d", d3.line().x(d => { return x(d[0])})
     .y(d => {return y(d[1])})
@@ -99,12 +102,52 @@ svg.append("g").call(d3.axisLeft(y))
     svg.append("path")
     .datum(seasonPoints["Southampton"])
     .attr("class", "line")
+    .attr("id", "line1")
     .style("stroke", "yellow")
     .attr("d", d3.line().x(d => { return x(d[0]) })
     .y(d => {return y(d[1]) })
     )
+    
+    d3.selectAll(".line").each((d, i) => {
+        var totalLength = d3.select("#line" + i).node().getTotalLength();
+        
+        d3.selectAll("#line" + i).attr("stroke-dasharray", totalLength + " " + totalLength)
+        .attr("stroke-dashoffset", totalLength)
+        .transition()
+        .duration(4000)
+        .delay(100*i)
+        .ease(d3.easeSin)
+        .attr("stroke-dashoffset", 0)
+    })
   
 
+  
+  
+    svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left - 5)
+    .attr("x", 0 - (height/2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .attr("class", "yaxis")
+    .text("Points")
+    .attr("font-size", "22px")
+    .style('fill', 'white')
+
+    svg.append("text")
+    .attr("transform", "translate (" + (width/2) + " ," + (height + margin.top + 25) + ")")
+    .attr("class", "xaxis")
+    .style("text-anchor", "middle")
+    .text("Matchday")
+    .attr("font-size", "22px")
+    .style('fill', 'white')
+
+    svg.append("circle").attr("cx", 100).attr("cy", 50).attr("r", 4).style("fill", "red")
+    svg.append("circle").attr("cx", 100).attr("cy", 70).attr("r", 4).style("fill", "yellow")
+    svg.append("text").attr("x", 120).attr("y", 50).style("fill", "white").text("Liverpool").attr("alignment-baseline","middle")
+    svg.append("text").attr("x", 120).attr("y", 70).style("fill", "white").text("Southampton").attr("alignment-baseline","middle")
+
+  
 
 });
 
